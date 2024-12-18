260601a0e1a8f8908be37ab5490e17a4fc1ce778
Sbharath001 <sbharath122002@gmail.com>
2024-12-18 15:20:34 +0530

    Update

--- Java console/src/DBConnection.java
+++ Java console/src/DBConnection.java
@@ -3,15 +3,12 @@ package bharath;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-public class DBConnection
-{
-	public static Connection connection=null;
-	public Connection getConnection()throws Exception
-	{
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/songdetails","root","Sbharath@01");
-		return connection;
-		
-	}
+public class DBConnection {
+    public static Connection connection = null;
 
+    public Connection get_connection() throws Exception { 
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/songdetails", "root", "Sbharath@01");
+        return connection;
+    }
 }
