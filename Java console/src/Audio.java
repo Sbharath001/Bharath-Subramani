f4bde6291df60309cf88f4c5c93071edccdf19c0
Sbharath001 <sbharath122002@gmail.com>
2024-12-18 15:19:37 +0530

    Update

--- Java console/src/Audio.java
+++ Java console/src/Audio.java
@@ -1,18 +1,17 @@
 package bharath;
 import java.util.*;
 
-public class Audio
-{
-	static Scanner scan = new Scanner(System.in);
-	public static void main(String args[])
-	{
-		Player click=new Player();
-		click.Play();
-		DBConnection dbconnection=new DBConnection();
-		try {
-		dbconnection.getConnection();
-		}catch(Exception e) {
-			System.out.println("SqlException");
-		}
-	}
+public class Audio {
+    static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Player click = new Player();
+        click.play(); 
+        DBConnection db_connection = new DBConnection();  
+        try {
+            db_connection.get_connection();  
+        } catch (Exception e) {
+            System.out.println("SqlException");
+        }
+    }
 }
