ece9b0a0d9a8a37b349abe6279616a632eb1dd20
Sbharath001 <sbharath122002@gmail.com>
2024-12-18 15:07:22 +0530

    Update

--- Java console/src/MusicPlayer.java
+++ Java console/src/MusicPlayer.java
@@ -5,87 +5,74 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Random;
 
-class MusicPlayer 
-{	
-	ArrayList<Song> musiclist;
-	Queue<Song> musicQ;
-	int total_Song_Count;
-	Random rand;
-	
-	MusicPlayer()
-	{
-		System.out.println("<<<<<< Initialize Music Player >>>>>>");
-		musiclist=new ArrayList<Song>();
-		rand = new Random();
-		musicQ=new LinkedList<Song>();
-		total_Song_Count=0;
-	}
-	
-	void addSong(int ID,String singer,String songname,Genre type)
-	{
-		musiclist.add(total_Song_Count,new Song(ID,singer,songname,type));
-		total_Song_Count++;
-	}
-	void playlist() {
-		System.out.println("Songs List-[");
-		for(int count=0;count<total_Song_Count;count++) {
-			Song song=(Song)musiclist.get(count);
-			System.out.println(song.song_name+", ");
-		}
-		System.out.println("]");
-	}
-	
-	void playRandomSong()
-	{
-		int rand_int = rand.nextInt(total_Song_Count); 
-		Song playingSong=musiclist.remove(rand_int);
-		total_Song_Count--;
-		musicQ.add(playingSong);
-		System.out.println("The song : "+playingSong.song_name+" is playing");
-		musiclist.add(playingSong);
-	}
-	
-	void playSong(int ID)
-	{
-		for(int count=0;count<total_Song_Count;count++)
-		{
-			Song x=(Song)musiclist.get(count);
-			if(x.songID==ID)
-			{
-				musiclist.remove(count);
-				total_Song_Count--;
-				System.out.println("The song : "+x.song_name+" is currently playing.");
-				musiclist.add(x);
-				return;
-			}
-		}
-		
-		for(int count=total_Song_Count;count<musiclist.size();count++)
-		{
-			Song x=(Song)musiclist.get(count);
-			if(x.songID==ID)
-			{
-					System.out.println("The song : "+x.song_name+" is currently playing.");
-			}
-		}
-		return;
-	}
-	void closeMusicPlayer()
-	{
-		System.out.println("<<<<<< Closing Music Player >>>>>>");
-		musicQ.clear();
-		total_Song_Count=musiclist.size();
-	}
-	
-	void playMusicFromQueue()
-	{
-		System.out.print("The current queue contains : [ ");
-		for(Song song:musicQ)
-		{
-			System.out.println(song.song_name+" ");
-		}
-		System.out.println("]");
-	}
-	
+class MusicPlayer {
+    ArrayList<Song> music_list;
+    Queue<Song> music_q;
+    int total_song_count;
+    Random rand;
+
+    MusicPlayer() {
+        System.out.println("<<<<<< Initialize Music Player >>>>>>");
+        music_list = new ArrayList<Song>();
+        rand = new Random();
+        music_q = new LinkedList<Song>();
+        total_song_count = 0;
+    }
+
+    void add_song(int id, String singer, String song_name, Genre type) {
+        music_list.add(total_song_count, new Song(id, singer, song_name, type));
+        total_song_count++;
+    }
+
+    void playlist() {
+        System.out.println("Songs List- [");
+        for (int count = 0; count < total_song_count; count++) {
+            Song song = music_list.get(count);
+            System.out.println(song.song_name + ", ");
+        }
+        System.out.println("]");
+    }
+
+    void play_random_song() {
+        int rand_int = rand.nextInt(total_song_count);
+        Song playing_song = music_list.remove(rand_int);
+        total_song_count--;
+        music_q.add(playing_song);
+        System.out.println("The song: " + playing_song.song_name + " is playing");
+        music_list.add(playing_song);
+    }
+
+    void play_song(int id) {
+        for (int count = 0; count < total_song_count; count++) {
+            Song x = music_list.get(count);
+            if (x.songID == id) {
+                music_list.remove(count);
+                total_song_count--;
+                System.out.println("The song: " + x.song_name + " is currently playing.");
+                music_list.add(x);
+                return;
+            }
+        }
+
+        for (int count = total_song_count; count < music_list.size(); count++) {
+            Song x = music_list.get(count);
+            if (x.songID == id) {
+                System.out.println("The song: " + x.song_name + " is currently playing.");
+            }
+        }
+    }
+
+    void close_music_player() {
+        System.out.println("<<<<<< Closing Music Player >>>>>>");
+        music_q.clear();
+        total_song_count = music_list.size();
+    }
+
+    void play_music_from_queue() {
+        System.out.print("The current queue contains: [ ");
+        for (Song song : music_q) {
+            System.out.println(song.song_name + " ");
+        }
+        System.out.println("]");
+    }
 }
-	
