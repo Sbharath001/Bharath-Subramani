1effbba57018991e5768821f7a507d5ef27c4b67
Sbharath001 <sbharath122002@gmail.com>
2024-12-18 15:15:55 +0530

    Update

--- Java console/src/Volume.java
+++ Java console/src/Volume.java
@@ -1,26 +1,23 @@
 package bharath;
 
-class Volume extends choose
-{
-	static int sound=50;
-	public void Volumeup()
-	{
-		if(sound<=90)
-		{
-			sound= sound+10;
-			System.out.println("volume increased 10 = "+sound);
-		}
-		else
-			System.out.println("Maximum volume is 100");
-	}
-	public void Volumedown()
-	{
-		if(sound>=10)
-		{
-			sound-=10;
-			System.out.println("volume decreased 10 = "+sound);			
-		}
-		else
-			System.out.println("volume is 0 no sound");
-	}
+class Volume extends Choose {
+    static int sound = 50;
+
+    public void volume_up() {
+        if (sound <= 90) {
+            sound = sound + 10;
+            System.out.println("Volume increased by 10 = " + sound);
+        } else {
+            System.out.println("Maximum volume is 100");
+        }
+    }
+
+    public void volume_down() {
+        if (sound >= 10) {
+            sound -= 10;
+            System.out.println("Volume decreased by 10 = " + sound);
+        } else {
+            System.out.println("Volume is 0, no sound");
+        }
+    }
 }
